03cfa32501485cc32e8e009ce6064df7663691a5
hugocg21 <hugocastanogonzalez@gmail.com>
2024-08-19 18:06:42 +0200

    Environment updated

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  backendUrl: 'https://log-in-app-dee8693134d9.herokuapp.com' // Cambia esto a la URL de tu backend en Heroku
+  backendUrl: 'https://log-in-app-dee8693134d9.herokuapp.com/api/media' // Cambia esto a la URL de tu backend en Heroku
 };
