889157e1bad7b441064ea2e79cf59de293609318
hugocg21 <hugocastanogonzalez@gmail.com>
2024-08-21 17:00:37 +0200

    App module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth'; // Add this for authentication
 
 import { LoginComponent } from './components/login/login.component';
 import { MediaUploadComponent } from './components/media-upload/media-upload.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
-import { HttpClientModule } from '@angular/common/http';
 import { MediaModalComponent } from './components/media-modal/media-modal.component';
 
+import { HttpClientModule } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { environment } from './environments/environments';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -33,22 +32,9 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
     HttpClientModule,
     FontAwesomeModule,
     DragDropModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireStorageModule,
-  ],
-  providers: [
-    provideFirebaseApp(() =>
-      initializeApp({
-        apiKey: "AIzaSyAI9SgpPzfpkxRmVItpRmRE_pEtIfw_mMg",
-        authDomain: "lahcosahnuehtrah.firebaseapp.com",
-        projectId: "lahcosahnuehtrah",
-        storageBucket: "lahcosahnuehtrah.appspot.com",
-        messagingSenderId: "12190056168",
-        appId: "1:12190056168:web:4f20d3f75083c18a11135d",
-        measurementId: "G-QJSMLBCF19"
-      })
-    ),
-    provideAuth(() => getAuth()),
+    AngularFireModule.initializeApp(environment.firebaseConfig), // Initialize Firebase
+    AngularFireStorageModule, // For Firebase storage
+    AngularFireAuthModule, // For Firebase authentication
   ],
   bootstrap: [AppComponent],
 })
